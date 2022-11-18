a842797c84fa864374d1b4a8cc36887d34c5e03a
Akash-41 <118422768+Akash-41@users.noreply.github.com>
2022-11-18 11:25:41 +0530

    Add files via upload

---
+++ calculator05.py
@@ -0,0 +1,21 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+         return a + b
+
+# This function subtracts two numbers
+    def subtract(self, a, b):
+        return a - b
+
+# This function multiplies two numbers
+    def multiply(self, a, b):
+        return a * b
+
+# This function divides two numbers
+    def divide(self, a, b):
+        return a / b
+
+
